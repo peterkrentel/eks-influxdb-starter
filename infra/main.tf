78d5d1055950767c8519b1dbb54548b3cd85928e
Peter Krentel <peterkrentel@gmail.com>
2025-05-26 19:33:03 -0500

    pub endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -20,6 +20,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.32"
 
+  cluster_endpoint_public_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
